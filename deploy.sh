7e2f3d0cc1a81b73b6d78990ea75bf87786b89a6
Laterner <55022049+Laterner@users.noreply.github.com>
2026-06-24 20:16:46 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
-cd ./site
+# Создаём директорию для сайта
+sudo mkdir -p /var/www/fstbot.ru/static
 
-npm build
+# Копируем ваш index.html и другие файлы
+sudo cp -r ~/static/* /var/www/fstbot.ru/static/
+
+# Копируем index.html в корень (чтобы был доступен по /)
+# sudo cp /var/www/fstbot.ru/static/index.html /var/www/fstbot.ru/
+
+# Копируем код бота
+sudo mkdir -p /opt/fstbot
+sudo cp -r ~/laterner.github.io/* /opt/fstbot/
