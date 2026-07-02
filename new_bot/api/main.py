7e20e892c0322ae46f463279c8aa07f5d4023191
Laterner <55022049+Laterner@users.noreply.github.com>
2026-07-02 19:49:03 +0300

    rework template

--- new_bot/api/main.py
+++ new_bot/api/main.py
@@ -23,13 +23,6 @@ from auth import (
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Пути
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
-
-# Создаем папку templates если её нет
-os.makedirs(TEMPLATES_DIR, exist_ok=True)
-
 # Инициализация FastAPI
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -48,8 +41,18 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+# Пути
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+# Создаем папку templates если её нет
+os.makedirs(TEMPLATES_DIR, exist_ok=True)
+
 # Шаблоны
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
+# print(STATIC_DIR)
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 # Security
 security = HTTPBearer()
@@ -58,11 +61,54 @@ security = HTTPBearer()
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="stub.html", context={"title": "Добавление очков"}
+    )
+
+@app.get("/miniapp", response_class=HTMLResponse)
+async def home(request: Request):
+    user = await db.get_user_by_player_id("SUHNG")
+    
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"title": "Home", 'user':user}
+    )
+
+@app.get("/qr", response_class=HTMLResponse)
+def qr_page(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="qr.html", context={"title": "QR"}
+    )
+
+@app.get("/quize", response_class=HTMLResponse)
+def quize_page(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="quize.html", context={"title": "Quize"}
+    )
+
+@app.post("/answer")
+def answer(ans: int ):
+    return {'ans':ans}
+
+@app.get("/faq", response_class=HTMLResponse)
+def faq_page(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="faq.html", context={"title": "faq"}
+    )
+
+@app.get("/map", response_class=HTMLResponse)
+def map_page(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="map.html", context={"title": "map"}
+    )
+
+""" Админка роуты """
+@app.get("/admin/addscore", response_class=HTMLResponse)
+async def admin_addscore(request: Request):
     """Главная страница с формой добавления очков"""
     return templates.TemplateResponse(
         request=request, name="admin_add_points.html", context={"title": "Добавление очков"}
     )
-
+    
 @app.post("/api/add_score")
 async def add_score(
     player_id: str = Form(...),
