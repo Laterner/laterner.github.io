12fb6ffe72ebff5e4c385d7169c9767097ab0460
Laterner <55022049+Laterner@users.noreply.github.com>
2026-07-05 13:55:51 +0300

    Update utils.py

--- new_bot/api/utils.py
+++ new_bot/api/utils.py
@@ -24,30 +24,27 @@ def validate_init_data(init_data: str):
     data_check_string = "\n".join(
         f"{k}={v}" for k, v in sorted(data.items())
     )
-    # print("data_check_string GET :::::::::::>>", data)
+    
     # 2. secret key
     secret_key = hmac.new(
         b"WebAppData",
         BOT_TOKEN.encode(),
         hashlib.sha256
     ).digest()
-    # print("secret_key GET :::::::::::>>", data)
+    
     # 3. hash
     computed_hash = hmac.new(
         secret_key,
         data_check_string.encode(),
         hashlib.sha256
     ).hexdigest()
-    # print("computed_hash GET :::::::::::>>", data)
 
     # if computed_hash != received_hash:
-    #     print("NONE computed_hash :::::::::::>> received_hash NONE")
     #     return None
 
     # 4. проверка времени (24h)
     # auth_date = int(data.get("auth_date", 0))
     # if time.time() - auth_date > 86400:
     #     return None
-    # print("auth_date GET :::::::::::>>", data)
 
     return data
