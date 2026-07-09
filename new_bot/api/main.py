36d1b152e00195632303d220c8b9c21e53cfb495
Laterner <55022049+Laterner@users.noreply.github.com>
2026-07-10 00:11:23 +0300

    Update main.py

--- new_bot/api/main.py
+++ new_bot/api/main.py
@@ -331,9 +331,9 @@ async def secretcode():
         if is_quiz_completed:
             return jsonify({'completed': 'completed'}) 
         
-        print(f"Quiz ID: {quize.id} {quize.secret_code}")
+        print(f"Quiz ID: {quize['id']} {quize.secret_code}")
         
-        return jsonify({'secret_code': secret_code})
+        return jsonify({'quize_id': quize['id'], 'secret_code': secret_code})
         
     except Exception as e:
         print(f"Error: {e}")
